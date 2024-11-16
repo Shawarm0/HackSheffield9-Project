9d9512e3f93c3a3656bd3dba7a998fcd894f500c
Lovin Sharma <loving.shawarma@gmail.com>
2024-11-16 21:53:43 +0000

    Text that is returned is filled into the text box

--- controllers/dyslexia/views.py
+++ controllers/dyslexia/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from controllers.utils import send_to_gemini, text_to_speech
 import threading
 
@@ -7,18 +7,26 @@ dyslexia_bp = Blueprint("dyslexia", __name__, template_folder="templates")
 def handle_text_to_speech(text):
     text_to_speech(text)
 
-
 @dyslexia_bp.route("/dyslexia", methods=["GET", "POST"])
 def dyslexia():
     if request.method == "POST":
-        text = request.form.get("text")# Retrieve the text from the form
-        processed_text = send_to_gemini("Can you take the text below and create a big story from it as detailed as possible", f"{text}") 
-        
+        data = request.get_json()  # Get JSON data from the request
+        text = data.get("text")  # Extract the 'text' key
+
+        # Process the text
+        processed_text = send_to_gemini(
+            "Can you take the text below and create a big story from it as detailed as possible",
+            f"{text}"
+        )
+
+        # Start the text-to-speech process in a separate thread
         tts_thread = threading.Thread(target=handle_text_to_speech, args=(processed_text,))
         tts_thread.start()
-        
-        return render_template("dyslexia/dyslexia.html", text=processed_text)  # Acknowledge the request
 
-    # Default response for GET requests
+        # Return the processed text as JSON
+        return jsonify({"processed_text": processed_text})
+
+    # Render the default template for GET requests
     return render_template("dyslexia/dyslexia.html")
+
 
