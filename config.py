5ec5136988915a3fffed1b2392fe794ee9db1deb
XDylanT <151638185+XDylanT@users.noreply.github.com>
2024-11-16 18:50:45 +0000

    dyslexia page is displaying correctly.[D

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
 
 app.config["WTF_CSRF_ENABLED"] = False
 
+app.config["WTF_CSRF_ENABLED"] = False
+
 # blueprints config - routing
 from controllers.dyspraxia.views import dyspraxia_bp
 from controllers.dyslexia.views import dyslexia_bp
