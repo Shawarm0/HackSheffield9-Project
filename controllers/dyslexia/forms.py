94b6b66c251b54fc2bc5f97f87f1df1e27d2dff9
XDylanT <151638185+XDylanT@users.noreply.github.com>
2024-11-16 18:42:12 +0000

    changes

--- controllers/dyslexia/forms.py
+++ controllers/dyslexia/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class DyslexiaForm(FlaskForm):
-    big_text = StringField(validators=[DataRequired()])
-    submit = SubmitField()
+    big_text = TextAreaField("Text to Convert", validators=[DataRequired()])
+    submit = SubmitField("Convert to Speech")
